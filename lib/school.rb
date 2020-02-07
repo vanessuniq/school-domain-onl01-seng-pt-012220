7f591a03527b96bfc920ff2f1311d086917c75a5
G. Vanessa Fotso <vanessuniq@gmail.com>
2020-02-07 06:49:06 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,21 @@
 # code here!
+class School
+  attr_reader :name, :roster, :school
+  def initialize (name, roster = {})
+    @name = name 
+    @roster = Hash.new {|h, k| h[k] = []}
+  end 
+  
+  def add_student (student_name, grade)
+    
+    @roster[grade] << student_name
+  end
+  
+  def grade(grade)
+    @roster[grade]
+  end
+  
+  def sort 
+    @roster.values.sort
+  end
+end
